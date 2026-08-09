475ec7ebf66795dcf35f557323b64f4da394895f
rjsgml04 <dbsrjsgml04@gmail.com>
2026-08-09 21:36:54 +0900

    문자 집합 조회

---
+++ src/charset/AvailableCharsetsMain.java
@@ -0,0 +1,43 @@
+package charset;
+
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
+import java.util.SortedMap;
+
+// 문자 집합 조회
+public class AvailableCharsetsMain {
+
+    public static void main(String[] args) {
+        // 이용 가능한 모든 Charset 자바 + OS
+        SortedMap<String, Charset> charsets = Charset.availableCharsets();
+        for (String charsetName : charsets.keySet()) {
+            System.out.println("charsetName = " + charsetName);
+        }
+
+        System.out.println("=====");
+        // 문자로 조회(대소문자 구분X) , MS949, ms949, x-windows-949 동일
+        Charset charset1 = Charset.forName("MS949");    // 원하는 타입을 꺼낼 수 있음
+        System.out.println("charset1 = " + charset1);
+
+        // 별칭 조회
+        Set<String> aliases = charset1.aliases();
+        for (String alias : aliases) {
+            System.out.println("alias = " + alias);
+        }
+
+        // UTF-8 문자로 조회
+        Charset charset2 = Charset.forName("UTF-8");
+        System.out.println("charset2 = " + charset2);
+
+        // UTF-8 상수로 조회
+        // 자주 사용하는 문자 집합은 상수로 정의되어 있음
+        Charset charset3 = StandardCharsets.UTF_8;
+        System.out.println("charset3 = " + charset3);
+
+        // 시스템의 기본 Charset 조회 - 시스템이 기본적으로 사용하는 문자 집합
+        Charset defaultCharset = Charset.defaultCharset();
+        System.out.println("defaultCharset = " + defaultCharset);
+    }
+}
