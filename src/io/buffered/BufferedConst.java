10fd113b23f09346569862f7fb7352b5c372ac41
rjsgml04 <dbsrjsgml04@gmail.com>
2026-08-16 15:23:31 +0900

    파일 설정

---
+++ src/io/buffered/BufferedConst.java
@@ -0,0 +1,7 @@
+package io.buffered;
+
+public class BufferedConst {
+    public static final String FILE_NAME = "temp/buffered.dat"; // 파일 경로
+    public static final int FILE_SIZE = 10 * 1024 * 1024; // 10MB - 파일 크기
+    public static final int BUFFER_SIZE = 8192; // 8KB - 파일 읽어들일 크기
+}
