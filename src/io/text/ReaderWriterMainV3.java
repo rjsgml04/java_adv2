0de3ef87daf3ced2c7515050385c0ed4012bd302
rjsgml04 <dbsrjsgml04@gmail.com>
2026-08-17 17:16:46 +0900

    문자다루기3 - Reader,Writer

---
+++ src/io/text/ReaderWriterMainV3.java
@@ -0,0 +1,38 @@
+package io.text;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static io.text.TextConst.FILE_NAME;
+import static java.nio.charset.StandardCharsets.*;
+
+public class ReaderWriterMainV3 {
+
+    public static void main(String[] args) throws IOException {
+        String writeString = "abc";
+        System.out.println("write String = " + writeString);
+
+        // 파일에 쓰기
+        /*
+        FileOutputStream fos = new FileOutputStream(FILE_NAME);
+        OutputStreamWriter osw = new OutputStreamWriter(fos, UTF_8);
+        두 코드를 간단하게 사용할 수 있음
+         */
+        FileWriter fw = new FileWriter(FILE_NAME, UTF_8);
+        fw.write(writeString);
+        fw.close();
+
+        // 파일 읽기
+        StringBuilder content = new StringBuilder();
+        FileReader fr = new FileReader(FILE_NAME, UTF_8);
+        int ch;
+        while((ch = fr.read()) !=-1){
+            content.append((char) ch);
+        }
+        fr.close();
+
+        String string = content.toString();
+        System.out.println("read String = " + string);
+    }
+}
